c20982902a10f319129df6ad5c957bbc57c113ad
Rakib Hassan <mailrakibhassan@gmail.com>
2025-06-09 09:35:51 +0600

    (fix) trying to fix cookie related problem in deployed version

--- src/app/modules/auth/auth.utils.ts
+++ src/app/modules/auth/auth.utils.ts
@@ -69,5 +69,6 @@ export const createSession = async (
     // session expiration time is in seconds
     // but maxAge needs to be in milliseconds
     maxAge: expiresIn * 1000 - 300000,
+    domain: '.onrender.com',
   });
 };
