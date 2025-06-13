81e5352f17be9642a6f11725b6d1f10386d011da
Rakib Hassan <mailrakibhassan@gmail.com>
2025-06-13 15:17:12 +0600

    (fix + feat) changes made in scripts

--- src/app/scripts/algolia.ts
+++ src/app/scripts/algolia.ts
@@ -1,24 +1,36 @@
-import { IndexSettings } from 'algoliasearch';
-import { client } from '../utils/algolia';
+//***** USE THE SCRIPT COMMAND FROM PACKAGE.JSON *****/
 
-const setAlgoliaIndexSettings = async (
-  indexName: string,
-  indexSettings: IndexSettings
-) => {
-  return client.setSettings({
-    indexName,
-    indexSettings,
-  });
-};
+// import { IndexSettings } from 'algoliasearch';
+// import { client } from '../utils/algolia';
+
+// const setAlgoliaIndexSettings = async (
+//   indexName: string,
+//   indexSettings: IndexSettings
+// ) => {
+//   return client.setSettings({
+//     indexName,
+//     indexSettings,
+//   });
+// };
 
 // set settings for any index
-setAlgoliaIndexSettings('habitUnits', {
-  searchableAttributes: ['type', 'unordered(name)'],
-  customRanking: ['desc(usageCount)'],
-})
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((reason: unknown) => {
-    console.log(reason);
-  });
+// setAlgoliaIndexSettings('habitUnits', {
+//   searchableAttributes: ['type', 'unordered(name)'],
+//   customRanking: ['desc(usageCount)'],
+// })
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((reason: unknown) => {
+//     console.log(reason);
+//   });
+
+// clear all records from an index without touching configs
+// client
+//   .clearObjects({ indexName: 'rewards' })
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err: unknown) => {
+//     console.log(`Error here =>> ${(err as Error).message}`);
+//   });
--- src/app/scripts/levels.ts
+++ src/app/scripts/levels.ts
@@ -1,43 +1,63 @@
-import { Level, RequirementLevel } from '../modules/level/level.model';
-import {
-  createLevels,
-  createRequirementsLevels,
-} from '../modules/level/level.util';
+//***** USE THE SCRIPT COMMAND FROM PACKAGE.JSON *****/
 
-export const seedLevels = async () => {
-  // check here levels existence and not allow creating levels again
-  const totalLevels = await Level.estimatedDocumentCount();
+// import mongoose from 'mongoose';
+// import { Level, RequirementLevel } from '../modules/level/level.model';
+// import {
+//   createLevels,
+//   createRequirementsLevels,
+// } from '../modules/level/level.util';
+// import config from '../config';
 
-  if (totalLevels > 0) {
-    console.log(
-      'Levels are already seeded. If you want fresh levels then delete requirementLevels and levels collection plus start the server again.'
-    );
-    return;
-  }
+// const seedLevels = async () => {
+//   // connect to mongodb first before doing the operations
+//   await mongoose.connect(config.db_connection_uri!);
 
-  try {
-    // create requirements with levels locally
-    const requirementsLevels = createRequirementsLevels();
+//   // check here levels existence and not allow creating levels again
+//   const totalLevels = await Level.estimatedDocumentCount();
 
-    // insert to db
-    // we have write into the db at this moment
-    // before calling the createLevels function which tries to get requirementLevel
-    // from the db
-    await RequirementLevel.insertMany(requirementsLevels);
+//   if (totalLevels > 0) {
+//     return {
+//       success: false,
+//       message:
+//         'Levels are already seeded. If you want fresh levels then delete requirementLevels and levels collection plus start the server again.',
+//     };
+//   }
 
-    // create levels locally to insert into db
-    const levels = await createLevels();
+//   try {
+//     // create requirements with levels locally
+//     const requirementsLevels = createRequirementsLevels();
 
-    // insert to db
-    await Level.insertMany(levels);
-  } catch (err) {
-    // drop the collection
-    console.log(
-      '*** Error happened. Dropping RequirementLevels & Levels collection. ***'
-    );
-    await RequirementLevel.collection.drop();
-    await Level.collection.drop();
+//     // insert to db
+//     // we have write into the db at this moment
+//     // before calling the createLevels function which tries to get requirementLevel
+//     // from the db
+//     await RequirementLevel.insertMany(requirementsLevels);
 
-    throw err;
-  }
-};
+//     // create levels locally to insert into db
+//     const levels = await createLevels();
+
+//     // insert to db
+//     await Level.insertMany(levels);
+
+//     // return a success response
+//     return { success: true, message: 'Successful seeding levels 🔥' };
+//   } catch (err) {
+//     // drop the collection
+//     console.log(
+//       '*** Error happened. Dropping RequirementLevels & Levels collection... ***'
+//     );
+//     await RequirementLevel.collection.drop();
+//     await Level.collection.drop();
+
+//     // the error will be catched inside catch block
+//     throw err;
+//   }
+// };
+
+// seedLevels()
+//   .then((res) => {
+//     console.log(res.message);
+//   })
+//   .catch((err: unknown) => {
+//     console.log(`Error here =>> ${(err as Error).message}`);
+//   });
